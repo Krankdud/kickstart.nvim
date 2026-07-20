88210a518685855f86453531da697d20c59467d1
charlie <krankdud@gmail.com>
2026-07-19 17:30:11 -0700

    expandtab

--- init.lua
+++ init.lua
@@ -83,6 +83,7 @@ vim.o.confirm = true
 -- Tab settings
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
+vim.o.expandtab = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -919,7 +920,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
